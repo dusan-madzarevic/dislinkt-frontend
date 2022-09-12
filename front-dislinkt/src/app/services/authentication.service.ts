2fda654c92180ec70eb14e868f53a9a92db7a85f
dusan-madzarevic <ducom98@gmail.com>
2022-09-12 20:16:14 +0200

    feat(auth): login connected to backend

--- front-dislinkt/src/app/services/authentication.service.ts
+++ front-dislinkt/src/app/services/authentication.service.ts
@@ -4,7 +4,8 @@ import { User } from '../models/user';
 import { map, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
-
+import { DateTime } from 'luxon';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,7 @@ import { environment } from 'src/environments/environment';
 export class AuthenticationService {
 
   private readonly API_REG: string = `${environment.baseUrl}/${environment.apiReg}`;
+  private readonly API_LOGIN: string = `${environment.baseUrl}/${environment.apiLogin}`;
 
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
@@ -28,4 +30,61 @@ export class AuthenticationService {
       map((res: any) => { return true; })).pipe(
         catchError((error: any) => {return throwError('Not created'); }));
   }
+
+  login(formGroup: FormGroup) {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+    })
+
+    let formData = new FormData();
+    console.log(formGroup.controls)
+    formData.append('username', formGroup.controls['email'].value)
+    formData.append('password', formGroup.controls['password'].value)
+    formData.append('grant_type', formGroup.controls['grant_type'].value)
+    formData.append('scope', formGroup.controls['scope'].value)
+    formData.append('client_id', formGroup.controls['client_id'].value)
+    formData.append('client_secret', formGroup.controls['client_secret'].value)
+
+
+    return this.http.post(this.API_LOGIN, formData)
+        .subscribe((res:any)=>{
+          this.setSession(res);
+        })
+  }
+      
+private setSession(authResult) {
+    let expireTime = DateTime.now().plus({minutes: authResult.expires_in_minutes})
+    
+    localStorage.setItem('access_token', authResult.access_token);
+    localStorage.setItem("expires_at", JSON.stringify(expireTime.toMillis()));
+}          
+
+logout() {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("expires_at");
+}
+
+public isLoggedIn() {
+  const expiration = localStorage.getItem("expires_at");
+  if(expiration != null){
+    return DateTime.now() < DateTime.fromMillis(JSON.parse(expiration));
+  }else{
+    return false;
+  }
+}
+
+isLoggedOut() {
+    return !this.isLoggedIn();
+}
+
+getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    if(expiration != null){
+      const expiresAt = JSON.parse(expiration);  
+      return DateTime.fromMillis(expiresAt);
+    }else{
+      return "error";
+    }
+} 
+
 }
