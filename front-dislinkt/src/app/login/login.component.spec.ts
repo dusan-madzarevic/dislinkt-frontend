f6cfa983967ad8230e836305e4146974037b867a
dusan-madzarevic <ducom98@gmail.com>
2022-09-13 23:01:02 +0200

    fix(test): added required modules for the create test to pass

--- front-dislinkt/src/app/login/login.component.spec.ts
+++ front-dislinkt/src/app/login/login.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
 import { HttpClient } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,7 +14,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ HttpClientTestingModule ]
+      imports: [ HttpClientTestingModule, RouterTestingModule, MatSnackBarModule ]
     })
     .compileComponents();
   });
