e184957f07ebc6cc25833e9ecc1f963835b29318
marijacurcic7 <marija.curcic07@gmail.com>
2022-09-18 20:46:53 +0200

    test: testing - part 1

--- front-dislinkt/src/app/home/home.component.spec.ts
+++ front-dislinkt/src/app/home/home.component.spec.ts
@@ -1,21 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
 import { HttpClient } from '@angular/common/http';
 import { RouterTestingModule } from "@angular/router/testing";
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+// import { Router } from '@angular/router';
+// import {Location} from "@angular/common";
+// import { LoginComponent } from '../login/login.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+  // let router: Router;
+  // let location: Location;
 
   beforeEach(async () => {
+    // const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    // const loginSpy = jasmine.createSpyObj('LoginComponent', ['login']);
     await TestBed.configureTestingModule({
       declarations: [ HomeComponent ],
-      imports: [ HttpClientTestingModule, RouterTestingModule, MatDialogModule, MatSnackBarModule ]
+      imports: [ HttpClientTestingModule, RouterTestingModule, MatDialogModule, MatSnackBarModule ],
+      // providers: [
+      //   {provide: LoginComponent, useValue: loginSpy}, {provide: Router, useValue: routerSpy}
+      // ]
     })
     .compileComponents();
   });
@@ -26,10 +36,27 @@ describe('HomeComponent', () => {
     httpClient = TestBed.inject(HttpClient)
     httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+    // router = TestBed.get(Router);
+    // location = TestBed.get(Location);
     
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#login redirects you to /login', fakeAsync(() => { 
+    // router.navigate(['login']);
+    // component.login();
+    // tick();
+
+    // const spy = router.navigate as jasmine.Spy;
+    // const navArgs = spy.calls.first().args[0];
+
+    // expect(location.path()).toBe('/login');
+
+  }));
+
+
 });
--- front-dislinkt/src/app/services/authentication.service.spec.ts
+++ front-dislinkt/src/app/services/authentication.service.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthenticationService } from './authentication.service';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { User } from '../models/user';
+import { Token } from '@angular/compiler';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
@@ -15,9 +17,73 @@ describe('AuthenticationService', () => {
     service = TestBed.inject(AuthenticationService);
     httpClient = TestBed.inject(HttpClient)
     httpTestingController = TestBed.inject(HttpTestingController);
+
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#signup should perform a signup', fakeAsync(() => {
+    let user : User = {
+      email: "admin@gmail.com", 
+      username: "admin", 
+      password: "123",
+      ime: "Ime",
+      prezime: "Prezime",
+      telefon: "123456",
+      datumRodjenja: "1999-01-07",
+      pol: "z"
+    };
+
+    let mockUser = {
+      email: "admin@gmail.com", 
+      username: "admin", 
+      password: "123",
+      ime: "Ime",
+      prezime: "Prezime",
+      telefon: "123456",
+      datumRodjenja: "1999-01-07",
+      pol: "z"
+    }
+
+    let res: boolean = false;
+
+    service.signup(user).subscribe(response => {
+      res = response;
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8001/register');
+    expect(req.request.method).toBe('POST');
+    req.flush(mockUser);
+
+    tick();
+
+    expect(res).toBeTruthy();
+  }));
+
+  it('#login should perform a post to /auth with email and password', fakeAsync(() => {
+    // let formData : FormData = {
+      
+    // }
+
+    // let res: Token;
+
+    // service.login(formData).subscribe(response => {
+    //   res = response;
+    // });
+
+    // const req = httpTestingController.expectOne('https://localhost:8081/token');
+    // expect(req.request.method).toBe('POST');
+    // req.flush(formData);
+
+    // tick();
+
+    // expect(res).toBeTruthy();
+  }));
+
+  it('#getUser should return current user', fakeAsync(() => {
+    
+  }));
+
 });
