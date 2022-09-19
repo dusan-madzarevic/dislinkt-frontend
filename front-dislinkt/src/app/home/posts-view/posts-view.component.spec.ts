e1b684cac4139d913c9374af6992edb5e870932d
Makim1998 <mmijatovic32@gmail.com>
2022-09-19 03:46:05 +0200

    feat: changing the structure of components

--- front-dislinkt/src/app/home/posts-view/posts-view.component.spec.ts
+++ front-dislinkt/src/app/home/posts-view/posts-view.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PostsViewComponent } from './posts-view.component';
 
@@ -8,7 +12,8 @@ describe('PostsViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostsViewComponent ]
+      declarations: [ PostsViewComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule, MatDialogModule, MatSnackBarModule ]
     })
     .compileComponents();
   });
--- front-dislinkt/src/app/home/posts-view/posts-view.component.ts
+++ front-dislinkt/src/app/home/posts-view/posts-view.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Post } from 'src/app/models/post';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { PostService } from 'src/app/services/post.service';
+import { UserService } from 'src/app/services/user.service';
+import { CreatePostComponent } from '../create-post/create-post.component';
 
 @Component({
   selector: 'app-posts-view',
@@ -7,9 +16,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public postService: PostService,
+    public authService: AuthenticationService,
+    public userService: UserService,
+    public dialog: MatDialog
+  ) { }
+
+  guest: Boolean;
+  posts: Post[];
+  search = false;
+  searchPending = false;
+  searchForm: FormGroup = new FormGroup({
+    profile: new FormControl('', [Validators.pattern(new RegExp('\\S'))])
+  });
+  foundUsers: User[] = [];
 
   ngOnInit(): void {
+    let user = this.authService.getUser();
+    if(!user){
+      this.guest = true;
+      console.log("neregistrovan korisnik");
+      this.getAllPosts();
+    }
+    else{
+      this.guest = false;
+      console.log("registrovan korisnik");
+      console.log(user.profile_id)
+      this.getFollowerPosts(user.profile_id);
+    }
   }
 
+  newPost(): void{
+    this.dialog.open(CreatePostComponent).afterClosed().subscribe(
+      result => {
+      console.log(result);
+      this.ngOnInit();
+    });
+    ;
+  }
+
+  getAllPosts(): void{
+    this.postService.fetchPosts().subscribe(
+      (posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  getUserPosts(user_id: number): void{
+    this.postService.fetchUserPosts(user_id).subscribe(
+      (posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  getFollowerPosts(profile_id: number): void{
+    this.postService.fetchFollowerPosts(profile_id).subscribe(
+      (posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  searchUsers(){
+  }
+
+  notSearch(){
+    this.search = false;
+  }
 }
