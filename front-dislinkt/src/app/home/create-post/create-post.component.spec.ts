db28834e93f24340cad355e12338215890f79dd2
marijacurcic7 <marija.curcic07@gmail.com>
2022-09-20 19:25:29 +0200

    test: testing new component

--- front-dislinkt/src/app/home/create-post/create-post.component.spec.ts
+++ front-dislinkt/src/app/home/create-post/create-post.component.spec.ts
@@ -1,14 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { CreatePostComponent } from './create-post.component';
 
 describe('CreatePostComponent', () => {
   let component: CreatePostComponent;
   let fixture: ComponentFixture<CreatePostComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreatePostComponent ]
+      declarations: [ CreatePostComponent ],
+      imports: [HttpClientTestingModule, MatSnackBarModule]
     })
     .compileComponents();
   });
@@ -16,6 +22,8 @@ describe('CreatePostComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreatePostComponent);
     component = fixture.componentInstance;
+    httpClient = TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
