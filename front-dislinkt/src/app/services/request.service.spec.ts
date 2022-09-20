d090b1576e646fba5ce239c0487be918828d798d
marijacurcic7 <marija.curcic07@gmail.com>
2022-09-20 16:02:22 +0200

    test: testing new components

--- front-dislinkt/src/app/services/request.service.spec.ts
+++ front-dislinkt/src/app/services/request.service.spec.ts
@@ -1,13 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { RequestService } from './request.service';
 
 describe('RequestService', () => {
   let service: RequestService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, MatSnackBarModule ]
+    });
     service = TestBed.inject(RequestService);
+    httpClient = TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
