db7a776652741697b786cf8fd7f228bbbadc9850
marijacurcic7 <marija.curcic07@gmail.com>
2022-09-20 13:31:02 +0200

    test: fix tests

--- front-dislinkt/src/app/layout/profile-short/profile-short.component.spec.ts
+++ front-dislinkt/src/app/layout/profile-short/profile-short.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProfileShortComponent } from './profile-short.component';
 
@@ -8,7 +9,8 @@ describe('ProfileShortComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileShortComponent ]
+      declarations: [ ProfileShortComponent ],
+      imports: [ RouterTestingModule ]
     })
     .compileComponents();
   });
